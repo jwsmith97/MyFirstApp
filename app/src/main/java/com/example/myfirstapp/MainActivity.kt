88db3e9283c60fafd4719e7967f0fbae550e3f19
JustinSmith <JustinSmith@Justins-MacBook-Pro.local>
2018-04-10 20:24:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+private_void testupload
+        iti=1+1
+        
